ec3bb6ea53cb3673ebb68b508e34ae2a88eedefe
Ho <love47024702@naver.com>
2023-10-27 01:01:40 +0900

    Refactor: emailSender 리팩토링

--- src/main/java/flab/gumipayments/infrastructure/sender/EmailSender.java
+++ src/main/java/flab/gumipayments/infrastructure/sender/EmailSender.java
@@ -24,19 +24,22 @@ public class EmailSender implements Sender {
     public void sendSignupRequest(String toAddress, String signupKey) {
         MimeMessage message = javaMailSender.createMimeMessage();
         try {
-            message.setFrom(id+"@naver.com");
-            message.setRecipients(MimeMessage.RecipientType.TO, toAddress);
-            message.setSubject("[구미 페이먼츠] 회원가입 인증 코드");
-            String body = "";
-            body += "구미 페이먼츠 인증 코드입니다." + "<br>";
-            body +="<a href=\"https://localhost:3000/api/accept/signup?signupKey="+signupKey+"&email="+toAddress+"\" >가입 인증</a>";
-            body += "<br>" +"지금 받은 이메일로 구미 페이먼츠를 가입하신 적이 없으면, 이메일을 무시해주세요.";
-
-            message.setText(body,"UTF-8", "html");
+            setSignupAcceptMessage(message,toAddress,signupKey);
             javaMailSender.send(message);
         } catch (MessagingException | MailException e) {
             log.error("error = ",e);
             throw new RuntimeException("다시 시도해주세요.");
         }
+    }
+
+    private void setSignupAcceptMessage(MimeMessage message, String toAddress, String signupKey) throws MessagingException {
+        message.setFrom(id+"@naver.com");
+        message.setRecipients(MimeMessage.RecipientType.TO, toAddress);
+        message.setSubject("[구미 페이먼츠] 회원가입 인증 코드");
+        String body = "";
+        body += "구미 페이먼츠 인증 코드입니다." + "<br>";
+        body +="<a href=\"https://localhost:3000/api/accept/signup?signupKey="+signupKey+"&email="+toAddress+"\" >가입 인증</a>";
+        body += "<br>" +"지금 받은 이메일로 구미 페이먼츠를 가입하신 적이 없으면, 이메일을 무시해주세요.";
+        message.setText(body,"UTF-8", "html");
     }
 }
