799a771ef274709ce14a832150b25c67dc18c756
HO <love47024702@naver.com>
2023-12-15 15:28:39 +0900

    Refactor: 시크릿 키 분리

--- src/main/java/flab/gumipayments/infrastructure/encrypt/KeyEncrypt.java
+++ src/main/java/flab/gumipayments/infrastructure/encrypt/KeyEncrypt.java
@@ -1,6 +1,7 @@
 package flab.gumipayments.infrastructure.encrypt;
 
 import org.apache.hc.client5.http.utils.Hex;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.Cipher;
@@ -10,23 +11,24 @@ import javax.crypto.spec.SecretKeySpec;
 @Component
 public class KeyEncrypt {
 
-    private static final String privateKey = "asdf;ghjklaxceghjas2djkla$sdfgha";
-    private static final String aesAlg = "AES/CBC/PKCS5Padding";
-    private static final String iv = privateKey.substring(0,16);
+    @Value("${encrypt.secret_key}")
+    private String privateKey;
+    @Value("${encrypt.aes_alg}")
+    private String aesAlg;
 
-    public String encrypt(String plainText){
+    public String encrypt(String plainText) {
+        String iv = privateKey.substring(0, 16);
         try {
-            SecretKeySpec secretKey = new SecretKeySpec(privateKey.getBytes("UTF-8"),"AES");
+            SecretKeySpec secretKey = new SecretKeySpec(privateKey.getBytes("UTF-8"), "AES");
             IvParameterSpec IV = new IvParameterSpec(iv.getBytes());
 
             Cipher c = Cipher.getInstance(aesAlg);
-            c.init(Cipher.ENCRYPT_MODE,secretKey,IV);
+            c.init(Cipher.ENCRYPT_MODE, secretKey, IV);
             byte[] encryptionByte = c.doFinal(plainText.getBytes("UTF-8"));
             return Hex.encodeHexString(encryptionByte);
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException("암호화 에러", e);
         }
-
     }
 }
