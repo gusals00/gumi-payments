3b5264b1bc4a684b132b724988cec32969ba2cb0
gusals <gusals6586@gmail.com>
2023-12-15 16:22:25 +0900

    Test: ApiKeyDecoder 테스트 작성

--- src/main/java/flab/gumipayments/presentation/PaymentsController.java
+++ src/main/java/flab/gumipayments/presentation/PaymentsController.java
@@ -2,10 +2,7 @@ package flab.gumipayments.presentation;
 
 import flab.gumipayments.apifirst.openapi.payments.domain.ApiKeyUseResponse;
 import flab.gumipayments.apifirst.openapi.payments.rest.PaymentsApi;
-import flab.gumipayments.infrastructure.apikey.ApiKeyExpiredException;
-import flab.gumipayments.infrastructure.apikey.ApiKeyInfo;
-import flab.gumipayments.infrastructure.apikey.ApiKeyNotFoundException;
-import flab.gumipayments.infrastructure.apikey.AuthenticatedApiKey;
+import flab.gumipayments.infrastructure.apikey.*;
 import flab.gumipayments.presentation.exceptionhandling.ExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +23,6 @@ public class PaymentsController implements PaymentsApi {
         return ResponseEntity.ok(convert(apiKeyInfo));
     }
 
-
     @ExceptionHandler(value = ApiKeyNotFoundException.class)
     public ResponseEntity<ExceptionResponse> notFoundExceptionHandler(ApiKeyNotFoundException e) {
         return ExceptionResponse.of(NOT_FOUND, HttpStatus.UNAUTHORIZED, e.getMessage());
@@ -35,6 +31,11 @@ public class PaymentsController implements PaymentsApi {
     @ExceptionHandler(value = ApiKeyExpiredException.class)
     public ResponseEntity<ExceptionResponse> expiredExceptionHandler(ApiKeyExpiredException e) {
         return ExceptionResponse.of(EXPIRED, HttpStatus.UNAUTHORIZED, e.getMessage());
+    }
+
+    @ExceptionHandler(value = ApiKeyFormatException.class)
+    public ResponseEntity<ExceptionResponse> formatExceptionHandler(ApiKeyFormatException e) {
+        return ExceptionResponse.of(BINDING, HttpStatus.UNAUTHORIZED, e.getMessage());
     }
 
     private ApiKeyUseResponse convert(ApiKeyInfo apiKeyInfo) {
