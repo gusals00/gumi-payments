01b3df3ac94c7029e708a13cbb873515b325ad06
HO <love47024702@naver.com>
2024-02-05 22:11:39 +0900

    Refactor: apiKeyArgumentResolver 수정

--- payments/src/main/java/flab/gumipayments/domain/ApiKeyRepository.java
+++ payments/src/main/java/flab/gumipayments/domain/ApiKeyRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 public interface ApiKeyRepository extends JpaRepository<ApiKey, Long> {
 
     Optional<ApiKey> findBySecretKey(String secretKey);
+
+    Optional<ApiKey> findBySecretKeyOrClientKey(String key);
 }
